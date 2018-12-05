e705cdfe737f2441f4dc16ab97b02d59ddcf7974
98765kjh <98765kjh@gmail.com>
2018-12-05 14:09:36 +0900

    머클 트리에 노드 추가 (addNode) 및 디버깅용 트리 상태 출력 함수 구현

--- lib/merkle_lib.hpp
+++ lib/merkle_lib.hpp
@@ -6,12 +6,30 @@
 #define WORKSPACE_MERKLE_LIB_HPP
 
 #include <vector>
+#include <string>
+#include <stack>
+#include <stdio.h>
 #include <stdlib.h>
 #include <sha256/sha256.h>
+using namespace std;
 
-// TODO: _TREE_DEPTH 값을 64, 또는 128, 256 으로 변경
-#define _TREE_DEPTH 8
+// TODO: define 값 변경
+#define _TREE_DEPTH 3
+#define _SHA256_SPLIT 2
 typedef unsigned int uint;
+typedef unsigned long long int ullint;
+
+/////////////////// test data /////////////////////
+struct test_data {
+    int uid;
+    string user_name;
+    string var_name;
+    string var_type;
+    int var_value;
+};
+
+test_data null_data = { -1, "0", "0", "0", -1 };
+//////////////////////////////////////////////////
 
 namespace gruut {
     class MerkleNode {
@@ -19,22 +37,311 @@ namespace gruut {
         MerkleNode *m_left;
         MerkleNode *m_right;
         uint m_suffix;    // TODO: 비트 확장
-        uint m_value;     // TODO: 비트 확장
+        string m_value;     // TODO: 비트 확장
+        uint m_path;      // 노드 분기 시 필요할 것으로 예상됨
+        int m_uid;          // 테스트 용도
+        int m_suffix_len;
+        //MerkleNode *m_next;
+
+        string makeValue(test_data data) {
+            string key = to_string(data.uid) + data.user_name + data.var_name + data.var_type + to_string(data.var_value);
+            string value = sha256(key);
+
+            return value;
+        }
+        uint makePath(test_data data) {
+            string key = to_string(data.uid) + data.user_name + data.var_type;
+            string path = sha256(key);
+
+            path = path.substr(0, _SHA256_SPLIT);
+            return (uint) strtoul(path.c_str(), 0, 16);
+        }
+
     public:
-        MerkleNode();
+        MerkleNode(test_data data = null_data)
+        {
+            m_left = nullptr;
+            m_right = nullptr;
+            //m_next = nullptr;
+            m_suffix = 0;
+            m_value = makeValue(data);
+            //m_path = makePath(data);
+            m_path = 0;
+            m_uid = data.uid;
+            m_suffix_len = -1;
+        }
+
+        void reHash()
+        {
+            string l_value, r_value;
+
+            if (getLeft() != nullptr) {
+                l_value = getLeft()->getValue();
+            }
+            if (getRight() != nullptr) {
+                r_value = getRight()->getValue();
+            }
+
+            if (l_value.empty()) {
+                m_value = sha256(r_value);
+            }
+            else if (r_value.empty()) {
+                m_value = sha256(l_value);
+            }
+            else {
+                m_value = sha256(l_value + r_value);
+            }
+        }
+
+        bool isDummy()  { return (m_uid == -1); }
+        bool isLeaf()   { return ((m_uid != -1) && (m_suffix_len == 0)); }
+
+        /* setter */
+        void setLeft(MerkleNode *node)  { m_left  = node; }
+        void setRight(MerkleNode *node) { m_right = node; }
+        //void setNext(MerkleNode *node)  { m_next  = node;}
+        void setSuffix(uint _path, int pos)
+        {
+            uint mask = (uint) (1 << pos) - 1;
+            m_suffix = _path & mask;
+            m_suffix_len = pos;
+        }
+        void setPath(uint _path)        { m_path  = _path; }
+        void setUid(int _uid)        { m_uid  = _uid; }
+
+        /* getter */
+        MerkleNode* getLeft()   { return m_left; }
+        MerkleNode* getRight()  { return m_right; }
+        uint getSuffix()    { return m_suffix; }
+        string getValue()     { return m_value; }
+        uint getPath()      { return m_path; }
+        int getUid()        { return m_uid; }
+        int getSuffixLen()  { return m_suffix_len; }
+        //MerkleNode* getNext() { return m_next; }
+
     };
+
+
+
+
+
 
     class MerkleTree {
     private:
         MerkleNode *root;
-        uint m_path;      // path 를 가져와서 루트에서 탐색
+        ullint m_size;
 
-        void hashing(MerkleNode &node);
+        bool _debug_dir;
+        int _debug_depth;
+        string _debug_str_depth;
+        string _debug_str_dir;
+
+        bool getDirectionOf(uint path, int pos)
+        {
+            return (path & (1 << pos)) != 0;
+        }
+        char* intToBin(uint num) {
+            char *ret = new char(_SHA256_SPLIT + 2);
+
+            int pos = 0;
+            for (int i = _SHA256_SPLIT; i >= 0; --i) {
+                if ( (num & (1 << i)) == 0) {
+                    ret[pos] = '0';
+                }
+                else {
+                    ret[pos] = '1';
+                }
+                pos++;
+            }
+            ret[_SHA256_SPLIT + 1] = '\0';
+
+            return ret;
+        }
+        void visit(MerkleNode *node) {
+            //printf("2");
+            string str_dir = !_debug_dir ? "Left" : "Right";
+            if (!node->isDummy()) {
+                printf("%s%s\t", _debug_str_dir.substr(0, _debug_depth).c_str(), str_dir.c_str());
+                printf("[uid %-3d] path: %s\n", node->getUid(), intToBin(node->getPath()));
+            }
+            _debug_depth--;
+        }
+        void postOrder(MerkleNode *node) {
+            _debug_depth++;
+            //printf("1");
+            printf("%s[depth %3d]\n", _debug_str_depth.substr(0, _debug_depth*2).c_str(), _debug_depth);
+            if (node->getLeft() != nullptr) { _debug_dir = false; postOrder(node->getLeft()); }
+            if (node->getRight() != nullptr) { _debug_dir = true; postOrder(node->getRight()); }
+            visit(node);
+        }
+
     public:
-        MerkleTree();
-        void addNode();
-        void modifyNode();
+        MerkleTree()
+        {
+            root = new MerkleNode();
+            m_size = 0;
+        }
 
+        // TODO: DB 와 연동하여 완성한 이후에는 new_path 파라미터 제거
+        void addNode(uint new_path, test_data data) {
+            MerkleNode *node = new MerkleNode(data);
+            node->setPath(new_path);
+            addNode(new_path, node);
+        }
+
+        void addNode(uint new_path, MerkleNode *new_node)
+        {
+            MerkleNode *node = root;
+            MerkleNode *prev_node = nullptr;
+            stack<MerkleNode *> stk;
+
+            bool collision = false;
+            bool dir;
+            int dir_pos = _TREE_DEPTH - 1;
+
+            int depth = 0;
+            while(true) {
+                if (!node->isDummy()) {
+                    collision = true;
+                    break;
+                }
+
+                stk.push(node); // 노드 삽입 후 노드에서 머클 루트까지 re-hashing 용도
+
+                dir = getDirectionOf(new_path, dir_pos);
+                depth++;
+                if (!dir && (node->getLeft() == nullptr)) {
+                    node->setLeft(new_node);
+                    node->setSuffix(new_path, dir_pos);
+                    break;
+                }
+                else if (dir && (node->getRight() == nullptr)) {
+                    node->setRight(new_node);
+                    node->setSuffix(new_path, dir_pos);
+                    break;
+                }
+                else {
+                    if (!dir) {
+                        prev_node = node;
+                        node = node->getLeft();
+                    }
+                    else {
+                        prev_node = node;
+                        node = node->getRight();
+                    }
+                }
+
+                dir_pos--;
+            }
+            // 기존 노드와 충돌 났을 때, 기존 노드와 새 노드 모두 삽입 필요
+            if (collision) {
+                MerkleNode *old_node;
+                uint old_path;
+                MerkleNode *dummy;
+
+                old_node = node;
+                old_path = old_node->getPath();
+
+                // 먼저 충돌난 곳에 dummy 노드 생성해서 연결
+                dummy = new MerkleNode();
+                stk.push(dummy);
+                if (!getDirectionOf(new_path, dir_pos + 1))
+                    prev_node->setLeft(dummy);
+                else
+                    prev_node->setRight(dummy);
+                prev_node = dummy;
+
+                // 경로가 같은 횟수만큼 경로를 따라서 dummy 노드 생성 및 연결
+                for(int i = dir_pos; i >= 0; --i) {
+                    if (getDirectionOf(new_path, i) == getDirectionOf(old_path, i)) {
+                        dummy = new MerkleNode();
+                        stk.push(dummy);
+                        if (!getDirectionOf(new_path, i)) {
+                            prev_node->setLeft(dummy);
+                        }
+                        else {
+                            prev_node->setRight(dummy);
+                        }
+                        prev_node = dummy;
+                    }
+                    else {
+                        if (!getDirectionOf(new_path, i)) {
+                            prev_node->setLeft(new_node);
+                            prev_node->setRight(old_node);
+                        }
+                        else {
+                            prev_node->setRight(new_node);
+                            prev_node->setLeft(old_node);
+                        }
+                        break;
+                    }
+                }
+            }
+
+            MerkleNode *tmp;
+            while(!stk.empty()) {
+                tmp = stk.top();
+                stk.pop();
+
+                tmp->reHash();
+            }
+
+            m_size++;
+        }
+
+        void modifyNode(uint path, MerkleNode *node)
+        {
+
+        }
+        void removeNode(uint path)
+        {
+
+        }
+
+        MerkleNode* getMerkleNode(uint _path, string _value)
+        {
+            MerkleNode *node = root;
+            int depth = 0;
+            int _path_pos = _TREE_DEPTH;
+
+            while(true) {
+                if (depth > _TREE_DEPTH){
+                    return nullptr;
+                }
+                else if (node->getValue() == _value) {
+                    break;
+                }
+
+                // _path 를 보고 left, right 노드로 진행
+                if (!getDirectionOf(_path, _path_pos))    { node = node->getLeft(); }
+                else                        { node = node->getRight(); }
+                depth++;
+                _path_pos--;
+            }
+
+            return node;
+        }
+
+        // Debugging
+        void printTreePostOrder() {
+            printf("*********** print tree data by using post-order traversal... ************\n\n");
+            printf("node size: %lld\n\n", getSize());
+            _debug_depth = -1;
+            _debug_dir = false;
+            _debug_str_depth = _debug_str_dir = "";
+            for(int i = 0; i < _TREE_DEPTH*2; ++i) {
+                _debug_str_depth += " ";
+                _debug_str_dir += "\t";
+            }
+            postOrder(root);
+            printf("*********** finish traversal ***********\n");
+        }
+
+        // setter
+        void setSize(ullint _size) { m_size = _size; }
+
+        // getter
+        ullint getSize()   { return m_size; }
     };
 }
 
--- test/main.cpp
+++ test/main.cpp
@@ -1,84 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <mysql.h>
 #include <iostream>
-#include <json.hpp>
-#include <storage_lib.hpp>
 #include <merkle_lib.hpp>
-#include <sha256/sha256.h>
-
 using namespace std;
 
-json js[7] = {json::parse("{\"user_id\":\"mizno\", \"var_name\":\"balance\",\"value\":50000,\"type\":\"gru\"}"),
-              json::parse("{\"user_id\":\"kjh\", \"var_name\":\"balance\",\"value\":20000,\"type\":\"gru\"}"),
-              json::parse("{\"user_id\":\"mang\", \"var_name\":\"balance\",\"value\":30000,\"type\":\"gru\"}"),
-              json::parse("{\"user_id\":\"skmoon\", \"var_name\":\"balance\",\"value\":40000,\"type\":\"gru\"}"),
-              json::parse("{\"user_id\":\"sheperd\", \"var_name\":\"balance\",\"value\":40000,\"type\":\"gru\"}"),
-              json::parse("{\"user_id\":\"sunnyq\", \"var_name\":\"balance\",\"value\":50000,\"type\":\"gru\"}"),
-              json::parse("{\"user_id\":\"lhs\", \"var_name\":\"balance\",\"value\":30000,\"type\":\"gru\"}"), };
+
+struct test_data data[6] = {
+		{0, "a", "gru", "coin", 50},
+		{1, "b", "gru", "coin", 50},
+		{2, "c", "gru", "coin", 50},
+		{3, "d", "gru", "coin", 50},
+		{4, "e", "gru", "coin", 50},
+		{5, "f", "gru", "coin", 50}
+};
+
+uint path[6] = {
+		0,  // 000
+		2,  // 010
+		3,	// 011
+		4,  // 100
+		5,  // 101
+		6   // 110
+};
+
+#define _DATA_LEN 6
 
 int main() {
-    string input = "grape";
-    string output1 = sha256(input);
+    gruut::MerkleTree tree = gruut::MerkleTree();
 
-    cout << "sha256('"<< input << "'):" << output1 << endl;
+    for(int i = 0; i < _DATA_LEN; ++i) {
+        tree.addNode(path[i], data[i]);
+    }
+
+//    tree.addNode(path[0], data[0]);
+//    tree.addNode(path[1], data[1]);
+//    tree.addNode(path[2], data[2]);
+//    tree.addNode(path[3], data[3]);
+
+    tree.printTreePostOrder();
+
     return 0;
 }
-/*
-int main() {
-
-    gruut::StorageLib s_lib;
-    string smart_contract; // json data from smart contract
-    int amount;
-    string from, to, type;
-
-    s_lib.dbInit();
-    if(s_lib.getProblem() == 1) {
-        return 0;
-    }
-
-    smart_contract = "{\"amount\":10000,\"from\":\"mizno\",\"to\":\"mang\",\"type\":\"gru\"}";
-
-    s_lib.parseJsonFromSmartContract(smart_contract);
-
-    amount = s_lib.getAmount();
-    from = s_lib.getFrom();
-    to = s_lib.getTo();
-    type = s_lib.getType();
-
-    cout << endl;
-
-    // check user
-    s_lib.checkUserId(from);
-    s_lib.checkUserId(to);
-    if(s_lib.getProblem() == 1) {
-        return 0;
-    }
-
-    cout << endl;
-
-    // check user's balance and transfer is possible
-    s_lib.checkUserBalance(from, "from");
-    s_lib.checkUserBalance(to, "to");
-    if(s_lib.getProblem() == 1) {
-        return 0;
-    }
-
-    cout << endl;
-
-    // update the user_balance in DB
-    s_lib.updateUserBalance(from, "from");
-    s_lib.updateUserBalance(to, "to");
-
-    cout << "*** processed" << endl;
-    cout << endl;
-
-    // check user's balance
-    s_lib.checkUserBalance(from, "just check");
-    s_lib.checkUserBalance(to, "just check");
-
-    return 0;
-
-
-}
-*/
